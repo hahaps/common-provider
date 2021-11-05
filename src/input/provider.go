95aa1bfe23ac9b808187fc3a0996a6ae22a98434
Li Xipeng <lixipeng@prophetech.cn>
2021-11-05 20:47:38 +0800

    Add credential

--- src/input/provider.go
+++ src/input/provider.go
@@ -1,6 +1,16 @@
 package input
 
+
+type Credential struct {
+	SecretId   string                 `yaml:"secret_id"`
+	SecretKey  string                 `yaml:"secret_key"`
+	AccountId  string                 `yaml:"account_id"`
+	ConnectURL string                 `yaml:"connect_url"`
+	Extra      map[string]interface{} `yaml:"extra"`
+}
+
 type Resource interface {
+	Init(credential Credential)
 	Run(args ...interface{}) ([]interface{}, string, map[string]interface{}, error)
 }
 
